ac2c53912873933bd37389f463b4717c3cfccf64
clouderaalex <2209394481@qq.com>
2019-10-09 17:23:12 +0800

    Update KafkaProperties.scala

--- src/main/scala/com/baozun/sdk/kafka/connection/test/KafkaProperties.scala
+++ src/main/scala/com/baozun/sdk/kafka/connection/test/KafkaProperties.scala
@@ -5,7 +5,7 @@ package com.baozun.sdk.kafka.connection.test
   */
 object KafkaProperties {
   val TOPIC = "baozun"
-  val KAFKA_SERVER_URL = "10.88.26.29"
+  val KAFKA_SERVER_URL = "192.168.1.100"
   val KAFKA_SERVER_PORT = 9092
   val KAFKA_PRODUCER_BUFFER_SIZE: Int = 4 * 1024
   val CONNECTION_TIMEOUT = 100000
